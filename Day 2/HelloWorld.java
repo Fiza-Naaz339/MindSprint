f39a2f0f4641305507cda7163450a7309e2588c4
Fiza Naaz <116147326+Fiza-Naaz339@users.noreply.github.com>
2025-02-19 00:15:54 +0530

    Delete Day 2 directory

--- Day 2/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-class HelloWorld{
-	static String name = "MindSprint";
-	public static void main(String[] args){
-		System.out.println("Hello world");
-		System.out.println(name.toUpperCase());
-}
-}
