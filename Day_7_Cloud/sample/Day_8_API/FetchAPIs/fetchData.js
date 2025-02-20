7cf906aa984429e8e97f51049e9259ec377d5c73
Fiza Naaz <116147326+Fiza-Naaz339@users.noreply.github.com>
2025-02-21 01:29:42 +0530

    Delete Day_7_Cloud/sample/Day_8_API directory

--- Day_7_Cloud/sample/Day_8_API/FetchAPIs/fetchData.js
+++
@@ -1,6 +0,0 @@
-const response = fetch('https://jsonplaceholder.typicode.com/users')
-response
-.then(response=>response.json())
-.then(json=>console.log(json))
-.catch(err=>console.log(err))
-.finally(()=>console.log('Promise Executed'))
