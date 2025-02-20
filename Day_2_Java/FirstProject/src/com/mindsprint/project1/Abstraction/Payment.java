ded7400268f343fb0db7b252d9c1025990791ec5
Fiza Naaz <116147326+Fiza-Naaz339@users.noreply.github.com>
2025-02-20 23:25:26 +0530

    Update Payment.java

--- Day_2_Java/FirstProject/src/com/mindsprint/project1/Abstraction/Payment.java
+++ Day_2_Java/FirstProject/src/com/mindsprint/project1/Abstraction/Payment.java
@@ -1,42 +1,26 @@
-package com.mindsprint.project1.Abstraction;
+package FirstProject.src.com.mindsprint.project1.Abstraction;
 
 import java.util.Scanner;
 
+interface Paypal {
+    void pay(String method);
+}
 
+interface Razorpay {
+    void pay(String method);
+}
 
-interface Paypal{
-    public void pay();
-}
-interface Razorpay{
-    public void pay();
-}
-public class Api1 implements Paypal{
-    @Override
-    public void pay(){
-        if(method.equals("Paypal"))
-            System.out.println("Payment done using PayPal");
-    }
-}
-public class Api2 implements Razorpay{
-    @Override
-    public void pay(){
-        if(method.equals("Razorpay"))
-            System.out.println("Payment done using Razorpay");
-    }
-}
-public class Payment{
+public class Payment {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Select yor payment method:");
+        System.out.println("Select your payment method:");
         String method = sc.next();
-        if(method.equals("Razorpay")) {
+        if (method.equals("Razorpay")) {
             Razorpay rp = new Api2();
-            rp.pay();
-        }
-        else{
+            rp.pay(method);
+        } else {
             Paypal pal = new Api1();
-            pal.pay();
+            pal.pay(method);
         }
     }
-
 }
